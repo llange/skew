dabaac36c9180e3424bb06d03c5452b31b919fa1
Ludovic LANGE <llange@users.noreply.github.com>
2018-08-21 23:16:02 +0200

    Remove typo

--- skew/resources/aws/firehose.py
+++ skew/resources/aws/firehose.py
@@ -1,4 +1,4 @@
-o# Licensed under the Apache License, Version 2.0 (the "License"). You
+# Licensed under the Apache License, Version 2.0 (the "License"). You
 # may not use this file except in compliance with the License. A copy of
 # the License is located at
 #
